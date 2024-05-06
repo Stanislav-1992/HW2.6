b98b18663874f5494a04bceab02e1e89e20e1373
Stanislav <s.tyulkov@gmail.com>
2024-05-06 18:40:34 +0300

    HW 2.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> words = new ArrayList<>(List.of("один", "один", "два", "три"));
+        printUnevenNumbers(nums);
+        printEvenNumbers(nums);
+        printingUniqueWords(words);
+        printingDuplicates(words);
+    }
+
+
+    private static void printUnevenNumbers(List<Integer> list) {
+        System.out.println("Задача 1");
+        for (Integer number : list) {
+            if (number % 2 != 0) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printEvenNumbers(List<Integer> list) {
+        System.out.println("Задача 2");
+        for (Integer number : list) {
+            if (number % 2 == 0)
+                System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printingUniqueWords(List<String> list) {
+        System.out.println("Задача 3");
+        Set<String> set = new TreeSet<>(list);
+        for (String word : set) {
+            System.out.print(word + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printingDuplicates(List<String> list) {
+        System.out.println("Задача 4");
+        Set<String> set = new HashSet<>(list);
+        for (String word : set) {
+            System.out.println(word + " = " + Collections.frequency(list, word));
+        }
     }
 }
